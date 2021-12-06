268da80b06369e668ede9c1c812ced3fd63730eb
Ante Baric <ante@kickass.website>
2021-12-06 13:34:09 +0100

    feat(day-6): part 2

--- day_6.rs
+++ day_6.rs
@@ -1,29 +1,45 @@
+use std::collections::HashMap;
+
 fn main() -> Result<(), reqwest::Error> {
     let input = reqwest::blocking::get("https://pastebin.com/raw/1KzMjj2U")?.text()?;
 
-    let mut fish_school: Vec<isize> = input
+    let fish_school: Vec<usize> = input
         .split(",")
         .map(|item| item.trim().parse().expect("parse failed"))
         .collect();
-    let simulation_days = 80;
+    let simulation_days = 256;
+    let mut spawns: HashMap<usize, usize> = HashMap::new();
 
-    for _ in 0..simulation_days {
-        let last_fish_school = fish_school.clone();
+    for fish in fish_school.iter() {
+        let spawn = spawns.entry(*fish + 1).or_insert(0);
 
-        for (index, fish) in last_fish_school.iter().enumerate() {
-            match fish {
-                0 => {
-                    fish_school[index] = 6;
-                    fish_school.push(8);
-                }
-                _ => {
-                    fish_school[index] -= 1;
-                }
-            }
-        }
+        *spawn += 1;
     }
 
-    println!("{}", fish_school.len());
+    let mut school_size = fish_school.len();
+
+    for day in 1..simulation_days + 1 {
+        let mut new_spawns = spawns.clone();
+
+        match spawns.get(&day) {
+            Some(new_fish) => {
+                let spawn = new_spawns.entry(day).or_insert(0);
+                *spawn = 0;
+
+                let spawn = new_spawns.entry(day + 1 + 6).or_insert(0);
+                *spawn += *new_fish;
+                school_size += new_fish;
+
+                let spawn = new_spawns.entry(day + 1 + 8).or_insert(0);
+                *spawn += new_fish;
+            }
+            None => (),
+        }
+
+        spawns = new_spawns;
+    }
+
+    println!("{}", school_size);
 
     Ok(())
 }
