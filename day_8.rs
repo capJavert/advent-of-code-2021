ad1863156f37aa38bbf47783eef2b43d2431742d
Ante Baric <ante@kickass.website>
2021-12-09 02:34:51 +0100

    feat(day-8): part 2

--- day_8.rs
+++ day_8.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct Entry {
     patterns: Vec<String>,
@@ -12,28 +15,101 @@ fn main() -> Result<(), reqwest::Error> {
         .lines()
         .map(|line| {
             let split: Vec<&str> = line.split(" | ").collect();
-            let patterns = split[0].split(" ").map(|item| String::from(item)).collect();
-            let outputs = split[1].split(" ").map(|item| String::from(item)).collect();
+            let patterns: Vec<String> = split[0]
+                .split(" ")
+                .map(|item| {
+                    let mut chars: Vec<char> = item.chars().collect();
+                    chars.sort();
+
+                    String::from_iter(chars)
+                })
+                .collect();
+            let outputs: Vec<String> = split[1]
+                .split(" ")
+                .map(|item| {
+                    let mut chars: Vec<char> = item.chars().collect();
+                    chars.sort();
+
+                    String::from_iter(chars)
+                })
+                .collect();
 
             Entry { patterns, outputs }
         })
         .collect();
-
-    let mut unique_output_count = 0;
+    let mut output_count = 0;
+    let segments: Vec<&str> = vec!["a", "b", "c", "d", "e", "f", "g"];
+    let digits: Vec<&str> = vec![
+        "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg",
+    ];
 
     for entry in entries.iter() {
-        for output in entry.outputs.iter() {
-            match output.len() {
-                2 => unique_output_count += 1,
-                4 => unique_output_count += 1,
-                3 => unique_output_count += 1,
-                7 => unique_output_count += 1,
-                _ => (),
+        for permutation in segments.iter().permutations(segments.len()).unique() {
+            let map: HashMap<String, String> =
+                permutation.iter().fold(HashMap::new(), |mut acc, item| {
+                    acc.insert(
+                        String::from(&segments[acc.len()][..]),
+                        String::from(&item[..]),
+                    );
+
+                    acc
+                });
+
+            let patterns: Vec<String> = digits
+                .iter()
+                .map(|original_pattern| {
+                    let mut pattern: Vec<String> = original_pattern
+                        .chars()
+                        .map(|original_letter| String::from(&map[&original_letter.to_string()]))
+                        .collect();
+                    pattern.sort();
+
+                    pattern.join("")
+                })
+                .collect();
+
+            if patterns
+                .iter()
+                .all(|pattern| entry.patterns.contains(pattern))
+            {
+                let map: HashMap<String, String> =
+                    permutation.iter().fold(HashMap::new(), |mut acc, item| {
+                        acc.insert(
+                            String::from(&item[..]),
+                            String::from(&segments[acc.len()][..]),
+                        );
+
+                        acc
+                    });
+
+                let displayed_number =
+                    entry.outputs.iter().fold(String::from(""), |acc, output| {
+                        let mut pattern: Vec<String> = output
+                            .chars()
+                            .map(|original_letter| String::from(&map[&original_letter.to_string()]))
+                            .collect();
+                        pattern.sort();
+
+                        let digit = pattern.join("");
+                        let number = digits
+                            .iter()
+                            .position(|item| item == &digit)
+                            .unwrap()
+                            .to_string();
+
+                        acc + &number
+                    });
+
+                let result: usize = displayed_number.parse().expect("parse failed");
+
+                output_count += result;
+
+                break;
             }
         }
     }
 
-    println!("{}", unique_output_count);
+    println!("{}", output_count);
 
     Ok(())
 }
