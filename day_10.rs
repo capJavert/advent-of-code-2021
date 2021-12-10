a679036f65048f5bff2fad6782b008fb7e53d68a
Ante Baric <ante@kickass.website>
2021-12-10 13:00:57 +0100

    feat(day-10): part 2

--- day_10.rs
+++ day_10.rs
@@ -24,10 +24,12 @@ fn check_chunk_close(
 fn main() -> Result<(), reqwest::Error> {
     let input = reqwest::blocking::get("https://pastebin.com/raw/AMq24KMw")?.text()?;
 
-    let mut invalid_characters: Vec<char> = vec![];
+    let mut incomplete_lines: Vec<Vec<char>> = vec![];
 
     for line in input.lines() {
         let mut stack: Vec<char> = vec![];
+
+        let mut invalid_character: Option<char> = None;
 
         for character in line.trim().chars() {
             let checked_character = match character {
@@ -46,32 +48,55 @@ fn main() -> Result<(), reqwest::Error> {
 
             match checked_character {
                 Some(c) => {
-                    invalid_characters.push(c);
+                    invalid_character = Option::from(c);
                     break;
                 }
                 None => (),
             };
         }
 
-        // TODO incomplete lines
-        // if stack.len() > 0 {
-        //     println!("Invalid line trailing characters {:?}", stack);
-        // }
+        match invalid_character {
+            Some(_) => (),
+            None => {
+                if stack.len() > 0 {
+                    incomplete_lines.push(stack)
+                }
+            }
+        }
     }
 
-    let error_score = invalid_characters
-        .iter()
-        .fold(0, |acc, character| match character {
-            ')' => acc + 3,
-            ']' => acc + 57,
-            '}' => acc + 1197,
-            '>' => acc + 25137,
-            _ => {
-                panic!("Invalid character {}", character);
-            }
-        });
+    let mut completion_scores: Vec<usize> = incomplete_lines
+        .into_iter()
+        .map(|mut line| {
+            line.reverse();
 
-    println!("{}", error_score);
+            line.iter().fold(0, |acc, character| {
+                let completion_character = match character {
+                    '(' => ')',
+                    '[' => ']',
+                    '{' => '}',
+                    '<' => '>',
+                    _ => {
+                        panic!("Invalid character {}", character);
+                    }
+                };
+
+                match completion_character {
+                    ')' => (acc * 5) + 1,
+                    ']' => (acc * 5) + 2,
+                    '}' => (acc * 5) + 3,
+                    '>' => (acc * 5) + 4,
+                    _ => {
+                        panic!("Invalid character {}", completion_character);
+                    }
+                }
+            })
+        })
+        .collect();
+
+    completion_scores.sort();
+
+    println!("{:?}", completion_scores[completion_scores.len() / 2]);
 
     Ok(())
 }
