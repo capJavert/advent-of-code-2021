ac4e07d351ddaf6d50910c9d6f6f50992705a884
Ante Baric <ante@kickass.website>
2021-12-20 12:32:37 +0100

    feat(day-16): part 2

--- day_16.rs
+++ day_16.rs
@@ -24,11 +24,10 @@ fn binary_to_decimal(binary: &str) -> usize {
     isize::from_str_radix(binary, 2).unwrap() as usize
 }
 
-fn parse_packet<'a>(packet: &'a str, version_numbers: &mut Vec<usize>) -> &'a str {
-    let mut extra = "";
+fn parse_packet<'a>(packet: &'a str) -> (usize, (usize, usize)) {
+    let mut value = 0;
+    let mut end_index = 0;
 
-    let version = binary_to_decimal(&packet[0..3]);
-    version_numbers.push(version);
     let type_id = binary_to_decimal(&packet[3..6]);
 
     match type_id {
@@ -43,15 +42,12 @@ fn parse_packet<'a>(packet: &'a str, version_numbers: &mut Vec<usize>) -> &'a st
                     }
                     0 => {
                         literal += &rest[index + 1..index + 5];
-                        extra = &rest[index + 5..];
+                        end_index = 6 + index + 5;
+                        value = binary_to_decimal(&literal);
                         break;
                     }
                     _ => panic!("invalid literal group prefix '{}'", character),
                 }
-            }
-
-            if extra.len() >= 6 {
-                parse_packet(extra, version_numbers);
             }
         }
         _ => {
@@ -63,49 +59,88 @@ fn parse_packet<'a>(packet: &'a str, version_numbers: &mut Vec<usize>) -> &'a st
                     .to_string()
                     .parse()
                     .expect("parse failed");
+                let mut sub_packets = vec![];
 
                 match length_type_id {
                     0 => {
                         if packet.len() >= 22 {
                             let length = binary_to_decimal(&packet[7..22]);
-                            parse_packet(&packet[22..22 + length], version_numbers);
-                            extra = &packet[22 + length..];
-                            if extra.len() >= 6 {
-                                parse_packet(extra, version_numbers);
+                            let mut index = 22;
+
+                            while index < 22 + length {
+                                let (sub_index, sub_packet) = parse_packet(&packet[index..]);
+                                index += sub_index;
+                                sub_packets.push(sub_packet);
                             }
+
+                            end_index = index;
                         }
                     }
                     1 => {
-                        if packet.len() >= 18 {
-                            parse_packet(&packet[18..], version_numbers);
+                        let length = binary_to_decimal(&packet[7..18]);
+                        let mut index = 18;
+                        let mut count = 0;
+
+                        while count < length {
+                            let (sub_index, sub_packet) = parse_packet(&packet[index..]);
+                            index += sub_index;
+                            count += 1;
+                            sub_packets.push(sub_packet);
                         }
+
+                        end_index = index;
                     }
                     _ => panic!("invalid length type_id '{}'", length_type_id),
                 }
+
+                value = match type_id {
+                    0 => sub_packets.iter().map(|s| s.1).sum(),
+                    1 => sub_packets.iter().map(|s| s.1).product(),
+                    2 => sub_packets.iter().map(|s| s.1).min().unwrap(),
+                    3 => sub_packets.iter().map(|s| s.1).max().unwrap(),
+                    5 => {
+                        if sub_packets[0].1 > sub_packets[1].1 {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    6 => {
+                        if sub_packets[0].1 < sub_packets[1].1 {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    7 => {
+                        if sub_packets[0].1 == sub_packets[1].1 {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    _ => panic!("invalid type_id {}", type_id),
+                };
             }
         }
     }
 
-    extra
+    (end_index, (type_id, value))
 }
 
-fn version_checksum(input: &str) -> usize {
+fn packet_value(input: &str) -> usize {
     let mut packet = String::new();
-    let mut version_numbers = vec![];
 
     for character in input.chars() {
         packet += hex_to_binary(&character.to_string());
     }
 
-    parse_packet(&packet, &mut version_numbers);
-
-    version_numbers.iter().fold(0, |acc, item| acc + item)
+    parse_packet(&packet).1 .1
 }
 
 fn main() -> Result<(), reqwest::Error> {
     let input = reqwest::blocking::get("https://pastebin.com/raw/jtiP9GMV")?.text()?;
-
-    println!("{}", version_checksum(&input));
+    println!("{}", packet_value(&input));
 
     Ok(())
 }
